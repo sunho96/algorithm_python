8fa87826bec2518217787bdde8b415480ff28f3f
sunny <senho500@gmail.com>
2025-01-28 21:43:20 +0900

    2주차 - array, linked list, stack

---
+++ 2nd_week/prac.py
@@ -0,0 +1,41 @@
+def isPalindrome(ln):
+    arr = []
+    head = ln.head
+
+    if not head:
+        return True
+
+    node = head
+    while node:
+        arr.append(node.val)
+        node = node.next
+
+    while len(arr) > 1:
+        first = arr.pop(0)
+        last = arr.pop()
+        if first != last:
+            return False
+
+    return True
+
+
+def test_problem_stack(s):
+    pair = {
+        '}': '{',
+        ')': '(',
+        ']': '[',
+    }
+    opener = "({["
+    stack = []
+
+    for char in s:
+        if char in opener:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            top = stack.pop()
+            if pair[char] != top:
+                return False
+
+    return not stack
